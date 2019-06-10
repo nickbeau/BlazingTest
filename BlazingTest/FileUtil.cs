95cf3e33a838e91276e9ab42da0a232e56899dee
Nick Beaugeard <nickbeau@hotmail.com>
2019-06-11 09:11:12 +1000

    Updated

---
+++ BlazingTest/FileUtil.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.JSInterop;
+
+namespace BlazingTest
+{
+    public static class FileUtil
+    {
+        public static Task SaveAs(string filename, byte[] data, IJSRuntime runtime)
+            => runtime.InvokeAsync<object>(
+                "saveAsFile",
+                filename,
+                Convert.ToBase64String(data));
+    }
+}
